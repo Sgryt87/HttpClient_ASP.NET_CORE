005ced144172515cd8a1ebed6a0bf60126d1cfd5
Sergii Grytsaienko <sergii.grytsaienko@accenture.com>
2019-08-26 22:43:36 -0500

    adding HttpClient

---
+++ Movies.Client/Services/CRUDService.cs
@@ -0,0 +1,33 @@
+﻿using Movies.Client.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Client.Services
+{
+    public class CRUDService : IIntegrationService
+    {
+        private static HttpClient _httpClient = new HttpClient();
+
+        public CRUDService()
+        {
+            _httpClient.BaseAddress = new Uri("http://localhost:57863");
+            _httpClient.Timeout = new TimeSpan(0, 0, 30);
+        }
+        public async Task Run()
+        {
+            await GetResource();
+        }
+
+        public async Task GetResource()
+        {
+            var response = await _httpClient.GetAsync("api/movies");
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            var movies = JsonConvert.DeserializeObject<IEnumerable<Movie>>(content);
+        }
+    }
+}
